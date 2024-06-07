98d129871210e10e4e2e821c5b6b2ffd2074a813
putcharmaster <putcharmaster@gmail.com>
2024-06-07 16:08:26 +0200

    push_swap move.c

--- push_swap/src/move.c
+++ push_swap/src/move.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -10,4 +11,51 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/push_swap.h"
 
+void    move(t_stack **a, t_stack **b)
+{
+    int min;
+
+    min = find_min_value(*a);
+    //find index of value;
+
+}
+
+int     find_index(t_stack *a, int min)
+{
+    int index;
+
+    index = 0;
+    while (a)
+    {
+        if (a->value == min)
+            return (index);
+        index++;
+        a = a->next;
+    }
+}
+
+int    move_to_top_cost(t_stack **a, int index)
+{
+    int size;
+
+    size = stack_size(a);
+    if (index < size / 2)
+        return (index); // ra
+    else
+        return (size - index); // rra
+}
+
+void    move_to_top(t_stack **a, int index)
+{
+    int size;
+    size = stack_size(a);
+
+    if (index < size / 2)
+        while (index--)
+            ra(a);
+    else
+        while (index--)
+            rra(a);
+}
