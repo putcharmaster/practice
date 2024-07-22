042152fd044e9c26cffc763a62b56604b85a5571
sangha <putcharmaster@gmail.com>
2024-07-22 12:46:50 +0200

    rank3 exam practice 2207

---
+++ 2207/g2.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*dupp(char *str)
+{
+	int	i = 0;
+	char	*dup;
+	while (str[i])
+		i++;
+	dup = malloc(sizeof(char) * i + 1);
+	if (!dup)
+		return (NULL);
+	i = -1;
+	while (str[++i])
+		dup[i] = str[i];
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*get_next_line(int fd)
+{
+	int	i= 0;
+	int	r;
+	char	c;
+	char	buf[10000];
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while ((r = read(fd, &c, 1)) > 0)
+	{
+		buf[i++] = c;
+		if (c == '\n')
+			break ;
+	}
+	buf[i] = '\0';
+	if (r <= 0 && i == 0)
+		return (NULL);
+	return (dupp(buf));
+}
